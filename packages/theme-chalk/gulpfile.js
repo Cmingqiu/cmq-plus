d59266cd86ade20dca174d5ffcd1f395203f7e4c
chenmq <1075124372@qq.com>
2022-07-29 10:44:24 +0800

    gulp 打包sass

--- packages/theme-chalk/gulpfile.js
+++ packages/theme-chalk/gulpfile.js
@@ -3,22 +3,25 @@ const sass = require('gulp-dart-sass');
 const cssmin = require('gulp-cssmin');
 const autoprefixer = require('gulp-autoprefixer');
 
+const outputDir = 'lib';
 /**
  * 打包sass,css文件
  */
 function compile() {
-  return src('./src/*.sass')
+  return src(`./src/*.scss`)
     .pipe(sass.sync())
     .pipe(autoprefixer({}))
     .pipe(cssmin())
-    .pipe(dest('./lib'));
+    .pipe(dest(outputDir));
 }
 
 /**
  * 拷贝字体文件
  */
 function copyfont() {
-  return src('./packages/theme-chalk/src/fonts/**').pipe(cssmin()).pipe(dest('./lib/fonts'));
+  return src(`./src/fonts/**`)
+    .pipe(cssmin())
+    .pipe(dest(`${outputDir}/fonts`));
 }
 
 exports.build = series(compile);
